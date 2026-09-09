d6bb55bd40d98555d42a3935438b2a1372f0605d
Vlad Kolenyk <kolesnikvlad416@gmail.com>
2026-09-09 15:08:20 +0300

    feat: add function for evaulating free drop

--- lab1/Form_onedimen_array.Designer.cs
+++ lab1/Form_onedimen_array.Designer.cs
@@ -29,6 +29,14 @@
         private void InitializeComponent()
         {
             button_close = new Button();
+            dataGridView1 = new DataGridView();
+            Number = new DataGridViewTextBoxColumn();
+            s = new DataGridViewTextBoxColumn();
+            label1 = new Label();
+            textBox_time = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // button_close
@@ -41,19 +49,89 @@
             button_close.UseVisualStyleBackColor = true;
             button_close.Click += button_close_Click;
             // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Number, s });
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(256, 398);
+            dataGridView1.TabIndex = 1;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Number
+            // 
+            Number.HeaderText = "№";
+            Number.Name = "Number";
+            // 
+            // s
+            // 
+            s.HeaderText = "Відстань";
+            s.Name = "s";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(297, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(30, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Час:";
+            // 
+            // textBox_time
+            // 
+            textBox_time.Location = new Point(333, 23);
+            textBox_time.Name = "textBox_time";
+            textBox_time.Size = new Size(100, 23);
+            textBox_time.TabIndex = 3;
+            textBox_time.Text = "10";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(439, 26);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 15);
+            label2.TabIndex = 4;
+            label2.Text = "секунд";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(297, 76);
+            button1.Name = "button1";
+            button1.Size = new Size(86, 23);
+            button1.TabIndex = 5;
+            button1.Text = "Обрахувати";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
             // Form_onedimen_array
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(textBox_time);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
             Controls.Add(button_close);
             Name = "Form_onedimen_array";
             Text = "Form_onedimen_array";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button_close;
+        private DataGridView dataGridView1;
+        private Label label1;
+        private TextBox textBox_time;
+        private Label label2;
+        private Button button1;
+        private DataGridViewTextBoxColumn Number;
+        private DataGridViewTextBoxColumn s;
     }
 }
