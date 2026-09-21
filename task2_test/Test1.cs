e54adb0e5aed2b33183f7df28fce0047b5768bac
Vlad Kolenyk <kolesnikvlad416@gmail.com>
2026-09-21 17:47:51 +0300

    test: add tests for lab2 task2

---
+++ task2_test/Test1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using lab1.lab2;
+
+namespace task2_test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void Calculate_all_valid()
+        {
+            var controller = new lab2_task2_controller(1, 500);
+            int expected = 1666;
+            int actual = controller.calculate();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Calculate_negative_numbers()
+        {
+            var controller = new lab2_task2_controller(-500, -1);
+            int expected = -1666;
+            int actual = controller.calculate();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Calculate_first_boundary_greater()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new lab2_task2_controller(500, 1));
+
+        }
+    }
+}
